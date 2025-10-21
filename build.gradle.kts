9e4f0cc850971f62fbe413d57c22970a3d151723
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2025-10-21 20:02:45 +1100

    update runserver to 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin"s jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.4")
+        minecraftVersion("1.21.8")
     }
     shadowJar {
         val base = "io.github.md5sha256.recipeviewer.libraries"
